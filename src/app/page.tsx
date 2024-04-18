c6624227a2c838c2c3ea42c0208bfec959ed2850
AchmadWahyu <achwahyu.w@gmail.com>
2024-04-18 20:56:13 +0700

    fix: not sync real time after edit github gist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,36 @@ import TableView from './TableView';
 import { Data } from './data.types';
 
 async function getData(): Promise<Data> {
-  const res = await fetch(
-    'https://gist.githubusercontent.com/AchmadWahyu/173e65a0667ff93ac35b3961163be27e/raw/aw-food-price-list.json'
-  );
+  try {
+    const response = await fetch(
+      'https://api.github.com/gists/173e65a0667ff93ac35b3961163be27e',
+      {
+        cache: 'no-store',
+        next: {
+          revalidate: 0,
+        },
+      }
+    );
 
-  if (!res.ok) {
+    const result = await response.json();
+    
+    return JSON.parse(result.files['aw-food-price-list.json'].content);
+    
+  } catch (err) {
+    console.log('ERR: ', err);
     throw new Error('Failed to fetch data');
   }
-
-  return res.json();
 }
 
 export default async function Home() {
-  const { data } = await getData();
+  const result = await getData();
 
   return (
     <main>
       <div className="bg-yellow-300">
         <h1 className="text-xl font-bold text-center">Menu AW Food & Snack</h1>
       </div>
-      <TableView data={data} />
+      <TableView data={result.data} />
     </main>
   );
 }
